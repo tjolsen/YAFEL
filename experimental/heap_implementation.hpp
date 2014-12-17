9b9c5fce9093b3065b61ab01ad74a873a740b6fc
Tyler Olsen <tjolsen@mit.edu>
2014-12-17 11:00:26 -0500

    Heap insert() and extract() work properly.

--- experimental/heap_implementation.hpp
+++ experimental/heap_implementation.hpp
@@ -1,14 +1,17 @@
+//=========================================
 template <typename T>
 Heap<T>::Heap() {
   array.clear();
 }
 
+//=========================================
 template <typename T>
 void Heap<T>::insert(T item) {
   array.push_back(item);
   BubbleUp(array.size()-1);
 }
 
+//=========================================
 template<typename T>
 void Heap<T>::BubbleUp(unsigned node) {
   if(node==0)
@@ -24,8 +27,42 @@ void Heap<T>::BubbleUp(unsigned node) {
   return;
 }
 
+//=========================================
+template<typename T>
+T Heap<T>::extract() {
+  
+  T retval = array[0];
+  array[0] = array[array.size()-1];
+  array.pop_back();
+  BubbleDown(0);
+  
+  return retval;
+}
 
+//=========================================
+template<typename T>
+void Heap<T>::BubbleDown(unsigned node) {
+  
+  unsigned Li, Ri, min;
+  Li = 2*node + 1;
+  Ri = 2*node + 2;
+  min=node;
+  
+  if(Li < array.size() && array[Li]<array[min])
+    min = Li;
+  if(Ri < array.size() && array[Ri] < array[min])
+    min = Ri;
+  
+  if(min != node) {
+    T tmp = array[node];
+    array[node] = array[min];
+    array[min] = tmp;
+    
+    BubbleDown(min);
+  }
+}
 
+//=========================================
 template<typename T>
 void Heap<T>::print() {
 
