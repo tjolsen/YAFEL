c5b6ca8bce9c6a5893127af5c017d91d6d79ab5c
Tyler Olsen <tjolsen@mit.edu>
2015-11-03 11:34:53 -0500

    added sparse_utils

---
+++ include/lin_alg/sparse_utils.hpp
@@ -0,0 +1,18 @@
+#ifndef _YAFEL_SPARSE_UTILS_HPP
+#define _YAFEL_SPARSE_UTILS_HPP
+
+
+#include "yafel_globals.hpp"
+
+#include <tuple>
+
+YAFEL_NAMESPACE_OPEN
+
+typedef std::tuple<size_type, size_type, value_type> triplet;
+
+
+
+
+YAFEL_NAMESPACE_CLOSE
+
+#endif
