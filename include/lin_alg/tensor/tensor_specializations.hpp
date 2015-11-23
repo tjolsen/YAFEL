59b3d9956c94771e0d3f969622bab1339c6d6678
Tyler Olsen <tjolsen@mit.edu>
2015-11-23 13:29:33 -0500

    minor changes to actually use the contract<N> function. Cleaner syntax than construction object. Hopefully all optimized away?

--- include/lin_alg/tensor/tensor_specializations.hpp
+++ include/lin_alg/tensor/tensor_specializations.hpp
@@ -13,7 +13,8 @@ template<typename T1, typename T2, unsigned DIM, typename dataType>
 const TensorContraction<T1,T2,2,1,1,dataType>
 operator*(const TensorExpression<T1,DIM,2,dataType> &lhs,
 	  const TensorExpression<T2,DIM,1,dataType> &rhs) {
-  return TensorContraction<T1,T2,DIM,2,1,1,dataType>(lhs,rhs);
+  //return TensorContraction<T1,T2,DIM,2,1,1,dataType>(lhs,rhs);
+  return contract<1>(lhs,rhs);
 }
 
 // specialize operator* for rank-2 * rank-2 tensor expressions (contract 1)
@@ -21,7 +22,8 @@ template<typename T1, typename T2, unsigned DIM, typename dataType>
 const TensorContraction<T1,T2,2,2,1,dataType>
 operator*(const TensorExpression<T1,DIM,2,dataType> &lhs,
 	  const TensorExpression<T2,DIM,2,dataType> &rhs) {
-  return TensorContraction<T1,T2,DIM,2,2,1,dataType>(lhs,rhs);
+  //return TensorContraction<T1,T2,DIM,2,2,1,dataType>(lhs,rhs);
+  return contract<1>(lhs,rhs);
 }
 
 // specialize operator* for rank-4 * rank-2 tensor expressions (contract 2)
@@ -29,7 +31,8 @@ template<typename T1, typename T2, unsigned DIM, typename dataType>
 const TensorContraction<T1,T2,4,2,2,dataType>
 operator*(const TensorExpression<T1,DIM,4,dataType> &lhs,
 	  const TensorExpression<T2,DIM,2,dataType> &rhs) {
-  return TensorContraction<T1,T2,DIM,4,2,2,dataType>(lhs,rhs);
+  //return TensorContraction<T1,T2,DIM,4,2,2,dataType>(lhs,rhs);
+  return contract<2>(lhs,rhs);
 }
 
 
