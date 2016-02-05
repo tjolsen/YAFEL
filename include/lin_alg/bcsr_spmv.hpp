249ee6ec1bbdc162a13aba1726dc61bf58f97bf3
Tyler Olsen <tjolsen@mit.edu>
2016-02-05 08:39:24 -0500

    added bcsr_spmv. going to write some tests for it now.

---
+++ include/lin_alg/bcsr_spmv.hpp
@@ -0,0 +1,54 @@
+#ifndef __YAFEL_BCSR_SPMV
+#define __YAFEL_BCSR_SPMV
+
+#include "yafel_globals.hpp"
+#include "lin_alg/Vector.hpp"
+#include "lin_alg/sparse_bcsr.hpp"
+
+#include <cassert>
+#include <cstdlib>
+#include <iostream>
+
+YAFEL_NAMESPACE_OPEN
+
+template<unsigned BLOCK, typename dataType>
+Vector<dataType> bcsr_spmv(const sparse_bcsr<BLOCK, dataType> &A,
+                           const Vector<dataType> &x)
+{
+
+#ifndef _OPTIMIZED
+  assert(A.cols() == x.length() && "Error: bcsr_spmv dimension mismatch");
+#endif
+
+  Vector<dataType> b(A.rows(), 0);
+  std::size_t BLOCK2 = BLOCK*BLOCK;
+  
+  for(std::size_t brow=0; brow<A.rows(). ++brow) {
+    
+    std::size_t idxmin, idxmax;
+    idxmin = A.brow_ptr[brow];
+    idxmax = A.brow_ptr[brow+1];
+    
+    //accumulate
+    dataType accum[BLOCK] = { };
+    for(std::size_t idx=idxmin; idx<idxmax; ++idx) {
+      for(std::size_t i=0; i<BLOCK; ++i) {
+        for(std::size_t j=0; j<BLOCK; ++j) {
+          col = A.bcol_index[idx]*BLOCK;
+          accum[i] += A.data[idx*BLOCK2 + i*BLOCK + j]*x(col);
+        }
+      }
+    }
+    for(std::size_t i=0; i<BLOCK; ++i) {
+      b(BLOCK*brow + i) = accum[i];
+    }
+    
+  }
+
+  
+  return b;
+}
+
+YAFEL_NAMESPACE_CLOSE
+
+#endif
