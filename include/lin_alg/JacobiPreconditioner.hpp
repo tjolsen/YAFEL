0e26524381550af03936a25a15a0af0e9df650f7
Tyler Olsen <tjolsen@mit.edu>
2016-02-06 22:27:54 -0500

    updated jacobi preconditioner

--- include/lin_alg/JacobiPreconditioner.hpp
+++ include/lin_alg/JacobiPreconditioner.hpp
@@ -8,15 +8,43 @@
 
 YAFEL_NAMESPACE_OPEN
 
-class JacobiPreconditioner : public Preconditioner {
+template<dataType>
+class JacobiPreconditioner : public Preconditioner<JacobiPreconditioner<dataType>, dataType> {
 
 private:
-  Vector AdiagInv;
+  Vector<dataType> AdiagInv;
 
 public:
-  JacobiPreconditioner(const sparse_csr &A);
+
+  using size_type = typename sparse_csr<dataType>::size_type;
+  using value_type = typename sparse_csr<dataType>::value_type;
+  using reference = typename sparse_csr<dataType>::reference;
+
+  JacobiPreconditioner(const sparse_csr<dataType> &A) : AdiagInv(A.rows(), dataType(0))
+  {
+    bool flag;
+    for(size_type i=0; i<A.rows(); ++i) {
+      value_type aii = A(i,i,flag);
+      if(flag) {
+        AdiagInv(i) = value_type(1)/aii;
+      }
+      else{
+        std::cerr << "Warning: JacobiPreconditioner found 0.0 on diag of A." << std::endl;
+        std::cerr << "\tResults using this preconditioner may be bad." << std::endl;
+        std::cerr << "\tPlacing value_type(1) in the preconditioner at location: " << i << std::endl;
+      }
+    }
+  }
   
-  Vector MinvV(const Vector &rhs) const;
+  Vector<dataType> MinvV(const Vector<dataTYpe> &rhs) const {
+    
+    Vector<dataType> retvec(rhs.size(), value_type(0));
+    for(size_type i=0; i<rhs.size(); ++i) {
+      retvec(i) = rhs(i)*AdiagInv(i);
+    }
+    
+    return retvec;
+  }
 };
 
 YAFEL_NAMESPACE_CLOSE
