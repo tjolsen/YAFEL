726bf5566fe8b65ea3247a3c083c957f84eab4bb
Tyler Olsen <tjolsen@mit.edu>
2016-02-19 17:22:24 -0500

    abandoning CRTP elements. Not necessary, as it is not a bottleneck of code, and makes ElementFactory polymorphism difficult, as each CRTP base class is a distinct type. Need runtime polymorphism in this case.

--- include/element/Element.hpp
+++ include/element/Element.hpp
@@ -7,7 +7,7 @@
 #include "lin_alg/Matrix.hpp"
 #include "lin_alg/Vector.hpp"
 #include "lin_alg/tensor/Tensor.hpp"
-#include "mesh/Mesh.hpp"
+#include "mesh/GenericMesh.hpp"
 #include "utils/DoFManager.hpp"
 
 YAFEL_NAMESPACE_OPEN
@@ -16,8 +16,9 @@ template<unsigned NSD>
 class Element {
   
 public:
-  using size_type = typename Tensor<NSD,1>::size_type;
-
+  using coordinate_type = Tensor<NSD,1>;
+  using size_type = typename coordinate_type::size_type;
+  
 
   size_type n_spaceDim;
   size_type n_quadPoints;
@@ -27,15 +28,15 @@ public:
   size_type el_num;
   size_type nodes_per_el;
   
-  std::vector<double> gauss_weights;
-  std::vector<Tensor<NSD,1> > quad_points;
-  std::vector<Tensor<NSD,1> > xi_0;
+  std::vector<double> quad_weights;
+  std::vector<coordinate_type> quad_points;
+  std::vector<coordinate_type> xi_0;
   std::vector<Tensor<NSD,2> > jacobians;
   std::vector<double> detJ;
-  std::vector<Vector<double> > vals; //vector of n_qp Vectors. holds values of shape funcs at qp's
-  std::vector<Matrix<double> > grads; //vector of n_qp FullMatrix objects, hold grads of shape funcs at qp's
-  std::vector<size_type> element;
-  std::vector<Tensor<NSD,1> > nodal_coords;
+  std::vector<Vector<double> > shape_vals; //vector of n_qp Vectors. holds values of shape funcs at qp's
+  std::vector<Matrix<double> > shape_grads; //vector of n_qp Matrix objects, hold grads of shape funcs at qp's
+  std::vector<size_type> element_nodes;
+  std::vector<coordinate_type> nodal_coords;
   std::vector<size_type> global_dofs;
   DoFManager DOFM;
   
@@ -44,37 +45,151 @@ public:
 
   // Virtual functions, specialized in child classes
   virtual ~Element() {}
-  virtual double shape_value_xi(size_type node, const Vector &xi) const = 0;
-  virtual double shape_grad_xi(size_type node, size_type component, const Vector &xi) const = 0;
+  virtual double shape_value_xi(size_type node, const coordinate_cype &xi) const = 0;
+  virtual double shape_grad_xi(size_type node, size_type component, const coordinate_type &xi) const = 0;
 
   //Functions in base class
-  Matrix<double> calcJ_xi(const Tensor<NSD,1> &xi);
+  Matrix<double> calcJ_xi(const coordinate_type &xi);
   void calcJacobians(); // calcualte Jacobians at Gauss points and store in 
   void calcGrads(); // calculate shape function gradients (wrt spatial coords) and store in "grads[qpi](A, i)"
   void calcVals(); // calculate shape function values and store in "vals[qpi](A)"
-  void update_element(const Mesh & M, size_type elnum);
-  inline double JxW(size_type qpi) const { return gauss_weights[qpi]*detJ[qpi]; }
+  Tensor<NSD,2> calcJ_xi(const coordinate_type &xi);
+
+  template<typename MTYPE, unsigned MNSD>
+  void update_element(const GenericMesh<MTYPE,NSD> & M, size_type elnum);
+
+  inline double JxW(size_type qpi) const { return quad_weights[qpi]*detJ[qpi]; }
   
   
   //utility functions, might need to use in program so make public
   inline size_type getComp(size_type dof) const { return (dof % dof_per_node); }
   inline size_type getBase(size_type dof) const { return (dof/dof_per_node); }
   
-  double xval(size_type dof, const Vector & xi);
-  
+  double xval(size_type dof, const Vector<double> & xi);
   
 };
 
 
 //================================================================
 /*
- * Element Implementation starts here
+ * Element implementation starts here
  */
 //================================================================
+
+//---------------------------------------------------------------------
+template<unsigned NSD>
+Element<NSD>::Element(const DoFManager &dofm, size_type nqp, 
+		 size_type dofpe, int vtktype, size_type nodespe) :
+  n_spaceDim(NSD), 
+  n_quadPoints(nqp),
+  dof_per_el(dofpe), 
+  vtk_type(vtktype), 
+  nodes_per_el(nodespe),
+  DOFM(dofm)
+{
+  dof_per_node = dofm.getDofPerNode();
+}
+
+
+//---------------------------------------------------------------------
+template<unsigned NSD>
+Tensor<NSD,2> Element<NSD>::calcJ_xi(const coordinate_type &xi) {
+ 
+  Tensor<NSD,2> ret;
+
+  for(size_type i=0; i<n_spaceDim; ++i) {
+    for(size_type j=0; j<n_spaceDim; ++j) {
+      for(size_type A=0; A<nodes_per_el; ++A) {
+	ret(i,j) += shape_grad_xi(A, j, xi) * nodal_coords[A](i);
+      }
+    }
+  }
+  
+  return retMat;
+}
+
+//---------------------------------------------------------------------
+template<unsigned NSD>
+void Element<NSD>::calcJacobians() {
+  jacobians.clear();
+  detJ.clear();
+
+  for(size_type i=0; i<n_quadPoints; ++i) {
+    jacobians.emplace_back( calcJ_xi(quad_points[i]) );
+    
+    detJ.push_back(jacobians[i].det());
+  }
+
+}
+
+
+//---------------------------------------------------------------------
+template<unsigned NSD>
+void Element::calcGrads() {
+  
+  grads.clear();
+  
+  for(size_type qpi=0; qpi<n_quadPoints; ++qpi) {
+    Vector qp = quad_points[qpi];
+    
+    FullMatrix Jinv = jacobians[qpi].getInverse();
+    FullMatrix NG(nodes_per_el, n_spaceDim, 0.0);
+
+    for(size_type A=0; A<nodes_per_el; ++A) {
+      for(size_type j=0; j<n_spaceDim; ++j) {
+	for(size_type k=0; k<n_spaceDim; ++k) {
+	  NG(A,j) += shape_grad_xi(A,k,qp)*Jinv(k,j);
+	}
+      }
+    }
+    
+    grads.push_back(NG);
+  }
+}
+
+
+//---------------------------------------------------------------------
+template<unsigned NSD>
+void Element::calcVals() {
+  vals.clear();
+  for(size_type qpi=0; qpi<n_quadPoints; ++qpi) {
+    auto qp = quad_points[qpi];
+    Vector<double> V(nodes_per_el, 0.0);
+    for(size_type A=0; A<nodes_per_el; ++A) {
+      V(A) = shape_value_xi(A, qp);
+    }
+    vals.push_back(V);
+  }
+}
+
+
+
+//---------------------------------------------------------------------
+template<unsigned NSD, typename MTYPE, unsigned MNSD>
+void Element::update_element(const GenericMesh<MTYPE,MNSD> &M, size_type elnum) {
+
+  size_type Nnodes = M.elements[elnum].size();
+
+  global_dofs.clear();
+  nodal_coords.clear();
+  element_nodes = M.elements[elnum];
+
+  for(size_type nodeNum : element_nodes) {
+    nodal_coords.push_back(M.nodal_coords[nodeNum]);
+    for(size_type j=0; j<dof_per_node; ++j) {
+      size_type dofNum = DOFM.index(nodeNum, j);//nodeNum*dof_per_node + j;
+      global_dofs.push_back(dofNum);
+    }
+  }
+
+  calcJacobians();
+  calcGrads();
+  calcVals();
+}
 
 
 
 
 YAFEL_NAMESPACE_CLOSE
 
-#endif //#ifndef _YAFEL_ELEMENT_HPP
+#endif //#ifndef __YAFEL_ELEMENT_HPP
