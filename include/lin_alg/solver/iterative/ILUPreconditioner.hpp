005e343afc68f230e3939b5002f0628fe94b839b
Tyler Olsen <tjolsen@mit.edu>
2016-06-27 11:19:26 -0400

    ILU is working, and it factors 150*150 x 150*150 in just a few seconds. Going to try to speed up using a hash table

--- include/lin_alg/solver/iterative/ILUPreconditioner.hpp
+++ include/lin_alg/solver/iterative/ILUPreconditioner.hpp
@@ -25,32 +25,32 @@ template<typename dataType>
 class ILUPreconditioner : public Preconditioner<ILUPreconditioner<dataType>, dataType> {  
 
 public:
-  using container_type = sparse_csr<dataType>;
-  using value_type = typename sparse_csr<dataType>::value_type;
-  using reference =  typename sparse_csr<dataType>::reference;
-  using size_type = typename sparse_csr<dataType>::size_type;
+    using container_type = sparse_csr<dataType>;
+    using value_type = typename sparse_csr<dataType>::value_type;
+    using reference =  typename sparse_csr<dataType>::reference;
+    using size_type = typename sparse_csr<dataType>::size_type;
 
 
 
-  // construct an incomplete LU factorization with no extra fill-in.
-  // filling options could be added later, for S&G.
-  // At first, no pivoting will be used, as this tends to increase the
-  // bandwidth of resulting matrices arising from PDE numerical methods.
-  ILUPreconditioner(const container_type &A);
+    // construct an incomplete LU factorization with no extra fill-in.
+    // filling options could be added later, for S&G.
+    // At first, no pivoting will be used, as this tends to increase the
+    // bandwidth of resulting matrices arising from PDE numerical methods.
+    ILUPreconditioner(const container_type &A);
 
 
-  void solve(Vector<dataType> &rhs) const;
-  const container_type & getILU() {return ILU;}
+    void solve(Vector<dataType> &rhs) const;
+    const container_type & getILU() {return ILU;}
 
 
 private:
-  container_type ILU;
+    container_type ILU;
 
-  //backward substitution
-  void b_subst(Vector<dataType> &rhs) const;
+    //backward substitution
+    void b_subst(Vector<dataType> &rhs) const;
   
-  //forward substitution
-  void f_subst(Vector<dataType> &rhs) const;
+    //forward substitution
+    void f_subst(Vector<dataType> &rhs) const;
 };
 
 //=============================================================================
@@ -61,49 +61,88 @@ private:
 
 template<typename dataType>
 ILUPreconditioner<dataType>::ILUPreconditioner(const sparse_csr<dataType> &A) 
-  : ILU(A) 
+    : ILU(A) 
 {
 #ifndef _OPTIMIZED
     assert(A.rows() == A.cols() && "ILUPreconditioner dimension mismatch");
 #endif    
 
+    //loop over pivots
     for(size_type r=0; r<ILU.rows()-1; ++r) {
-      bool flag = true;
-      
-      value_type d = value_type(1)/ILU(r, r, flag);
-      
-      for(size_type i=r+1; i<ILU.rows(); ++i) {
 
-        reference A_ir = ILU(i,r,flag);
-        
-        if(!flag) continue; //skip if not in sparsity
-        
-        value_type e = d*A_ir;
-        A_ir = e;
-        
-        size_type idxmin = ILU.row_ptr[i];
-        size_type idxmax = ILU.row_ptr[i+1];
-        for(size_type idx=idxmin; idx<idxmax; ++idx) {
-          if(ILU.col_index[idx] > r) {
-            idxmin = idx;
-            break;
-          }
+      
+        //get pivot index and value
+        size_type idx_pivot = ILU.row_ptr[r];
+        value_type pivot(0);
+        for(size_type idx=ILU.row_ptr[r]; idx<ILU.row_ptr[r+1]; ++idx) {
+            size_type col = ILU.col_index[idx];
+            if(col == r) {
+                idx_pivot = idx;
+                pivot = ILU._data[idx];
+                break;
+            }
+        }
+      
+        if(pivot == 0) {
+            std::cerr << "\\[\\e[1;31m\\]Warning: ILUPreconditioner: Found zero-valued pivot element.\n"
+                      << "Continuing without eliminating with this pivot. \\[\\e[m\\]" << std::endl;
+            continue;
         }
 
-        for(size_type idx=idxmin; idx<idxmax; ++idx) {
-          size_type j = ILU.col_index[idx];
-          reference A_ij = ILU._data[idx];
-          value_type A_rj = ILU(r,j,flag);
-          
-          if(flag) {
-            A_ij -= e*A_rj;
-          }
+        //eliminate below pivot
+        for(size_type i=r+1; i<ILU.rows(); ++i) {
 
-        } // end idx-loop
-        
-      } // end i-loop
+            size_type idx_ir = ILU.row_ptr[i];
+            bool in_sparsity(false);
+            value_type A_ir(0);
+            //get starting index in this row
+            for(size_type idx = ILU.row_ptr[i]; idx<ILU.row_ptr[i+1]; ++idx) {
+                size_type col = ILU.col_index[idx];
+                if(col == r) {
+                    idx_ir = idx;
+                    in_sparsity = true;
+                    A_ir = ILU._data[idx];
+                }
+                if(col > r) {
+                    break;
+                }
+            }
+
+            if(!in_sparsity) {
+                continue;
+            }
+
+            
+            //eliminate common elements in rows "r" and "i"
+            value_type m_ir = A_ir/pivot;
+            size_type idx_i = idx_ir;
+            size_type idx_r = idx_pivot;
+            size_type idx_imax = ILU.row_ptr[i+1];
+            size_type idx_rmax = ILU.row_ptr[r+1];
+            size_type col_r = ILU.col_index[idx_r];
+            size_type col_i = ILU.col_index[idx_i];
+
+            while(idx_i<idx_imax && idx_r<idx_rmax) {
+                //std::cout << "col_r = " << col_r << "    col_i = " << col_i << std::endl;
+                
+                if(col_i == col_r) {
+                    ILU._data[idx_i] -= m_ir*ILU._data[idx_r];
+                    col_i = ILU.col_index[++idx_i];
+                    col_r = ILU.col_index[++idx_r];
+                }
+                else if(col_i < col_r) {
+                    col_i = ILU.col_index[++idx_i];
+                }
+                else if(col_i > col_r) {
+                    col_r = ILU.col_index[++idx_r];
+                }
+                
+            }
+            ILU._data[idx_ir] = m_ir;
+
+        }// end i-loop
       
-    } // end r-loop
+    } // end r-loop (pivot loop)
 
 }// end ctor
 
@@ -113,8 +152,8 @@ ILUPreconditioner<dataType>::ILUPreconditioner(const sparse_csr<dataType> &A)
 template<typename dataType>
 void ILUPreconditioner<dataType>::solve(Vector<dataType> &rhs) const {
   
-  f_subst(rhs); // <---
-  b_subst(rhs); // <------- These functions modify the ret vector in place to eliminate excess copying
+    f_subst(rhs); // <---
+    b_subst(rhs); // <------- These functions modify the ret vector in place to eliminate excess copying
 
 }
 
