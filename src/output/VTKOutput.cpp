350a9ba1d558555e5d17b07a132b2e4cfae82966
Tyler Olsen <tjolsen@mit.edu>
2016-07-01 15:56:09 -0400

    changed perror() crap to throw an exception (std::runtime_error) from VTKOutput

--- src/output/VTKOutput.cpp
+++ src/output/VTKOutput.cpp
@@ -1,81 +1,80 @@
 #include "output/VTKOutput.hpp"
 #include <cstdlib>
 #include <cstdio>
+#include <stdexcept>
 
 YAFEL_NAMESPACE_OPEN
 
 VTKOutput::VTKOutput() :
-  vtkmesh(nullptr),
-  pointData(),
-  cellData()
+    vtkmesh(nullptr),
+    pointData(),
+    cellData()
 {}
 
 void VTKOutput::addVTKObject(VTKObject *VO) {
-  switch(VO->getObjectType()) {
-  case VTKObject::VTKMESH:
-    vtkmesh = VO;
-    break;
-  case VTKObject::VTKPOINTDATA:
-    pointData.push_back(VO);
-    break;
-  case VTKObject::VTKCELLDATA:
-    cellData.push_back(VO);
-    break;
-  }
+    switch(VO->getObjectType()) {
+    case VTKObject::VTKMESH:
+        vtkmesh = VO;
+        break;
+    case VTKObject::VTKPOINTDATA:
+        pointData.push_back(VO);
+        break;
+    case VTKObject::VTKCELLDATA:
+        cellData.push_back(VO);
+        break;
+    }
 }
 
 void VTKOutput::clearData() {
-  pointData.clear();
-  cellData.clear();
+    pointData.clear();
+    cellData.clear();
 }
 
 void VTKOutput::clearMesh() {
-  vtkmesh = NULL;
+    vtkmesh = NULL;
 }
 
 void VTKOutput::write(const std::string &fname) {
   
-  FILE *fp = fopen(fname.c_str(), "w");
-  if(fp == NULL) {
-    perror("VTKOutput::write() : Could not open file.");
-    exit(1);
-  }
-
-  if(vtkmesh == NULL) {
-    perror("VTKOutput::write() : no mesh assigned.");
-    exit(1);
-  }
-  
-  fprintf(fp, "<?xml version=\"1.0\"?>\n");
-  fprintf(fp, "<VTKFile type=\"UnstructuredGrid\" version=\"0.1\">\n");
-  vtkmesh->write(fp);
-  
-  //write point data
-  if(pointData.size() > 0) {
-    fprintf(fp, "<PointData>\n");
-    for(unsigned i=0; i<pointData.size(); ++i) {
-      pointData[i]->write(fp);
+    FILE *fp = fopen(fname.c_str(), "w");
+    if(fp == NULL) {
+        std::runtime_error("VTKOutput::write() : Could not open file.");
     }
-    fprintf(fp, "</PointData>\n");
-  }
 
-  //write cell data
-  if(cellData.size() > 0) {
-    fprintf(fp, "<CellData>\n");
-    for(unsigned i=0; i<cellData.size(); ++i) {
-      cellData[i]->write(fp);
+    if(vtkmesh == NULL) {
+        std::runtime_error("VTKOutput::write() : no mesh assigned.");
     }
-    fprintf(fp, "</CellData>\n");
-  }
-
-  //conclude file
-  fprintf(fp, "</Piece>\n");
-  fprintf(fp, "</UnstructuredGrid>\n");
-  fprintf(fp, "</VTKFile>");
-
-  fclose(fp);
   
-  fprintf(stdout, "VTK Output successfully written to %s\n", fname.c_str());
+    fprintf(fp, "<?xml version=\"1.0\"?>\n");
+    fprintf(fp, "<VTKFile type=\"UnstructuredGrid\" version=\"0.1\">\n");
+    vtkmesh->write(fp);
+  
+    //write point data
+    if(pointData.size() > 0) {
+        fprintf(fp, "<PointData>\n");
+        for(unsigned i=0; i<pointData.size(); ++i) {
+            pointData[i]->write(fp);
+        }
+        fprintf(fp, "</PointData>\n");
+    }
+
+    //write cell data
+    if(cellData.size() > 0) {
+        fprintf(fp, "<CellData>\n");
+        for(unsigned i=0; i<cellData.size(); ++i) {
+            cellData[i]->write(fp);
+        }
+        fprintf(fp, "</CellData>\n");
+    }
+
+    //conclude file
+    fprintf(fp, "</Piece>\n");
+    fprintf(fp, "</UnstructuredGrid>\n");
+    fprintf(fp, "</VTKFile>");
+
+    fclose(fp);
+  
+    fprintf(stdout, "VTK Output successfully written to %s\n", fname.c_str());
 }
 
 YAFEL_NAMESPACE_CLOSE
