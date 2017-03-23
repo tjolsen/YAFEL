31e26582004775e03080aa8c91160dcfc3b0ccc2
Tyler Olsen <tjolsen@mit.edu>
2017-03-23 18:50:43 -0400

    utility stuff

---
+++ include/element/ShapeFunctionUtils.hpp
@@ -0,0 +1,64 @@
+//
+// Created by tyler on 3/23/17.
+//
+
+#ifndef YAFEL_SHAPEFUNCTIONUTILS_HPP
+#define YAFEL_SHAPEFUNCTIONUTILS_HPP
+
+/**
+ * \file
+ * This file contains utilities for computing shape functions
+ * on tensor product and simplex elements. Functions included
+ * are:
+ *
+ * - Jacobi polynomials
+ * - Koornwinder polynomials
+ */
+
+
+#include "yafel_globals.hpp"
+#include <vector>
+
+YAFEL_NAMESPACE_OPEN
+
+/**
+ * \brief Evaluate a polynomial using Horner's Method
+ *
+ * Polynomial coeffs should be in decreasing order.
+ * i.e.: polynomials of degree "n" are evaluated as:
+ *
+ * \begin{equation}
+ * P(x) = coeffs[0]*x^{n} + coeffs[1]^x^{n-1} + ... + coeffs[n]
+ * \end{equation}
+ *
+ * @tparam T datatype of "x"
+ * @param coeffs coefficients of polynomial:
+ * @param x
+ * @return
+ */
+template<typename T>
+T poly_eval(const std::vector<double> &coeffs, T x)
+{
+    T retval;
+    for(int i=0; i<coeffs.size(); ++i) {
+        retval *= x;
+        retval += coeffs[i];
+    }
+    return retval;
+}
+
+/**
+ * \brief Create coefficients of n-th degree Jacobi polynomial
+ *
+ * @param n Jacobi polynomial degree
+ * @param alpha Jacobi alhpa
+ * @param beta Jacobi beta
+ * @return polynomial coefficients suitable for use in poly_eval
+ */
+std::vector<double> jacobi(int n, int alpha, int beta);
+
+
+YAFEL_NAMESPACE_CLOSE
+
+
+#endif //YAFEL_SHAPEFUNCTIONUTILS_HPP
