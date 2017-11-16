3a84c48b22a5a3bc96c44c481e3607787b17b75a
Tyler Olsen <tylerjohnolsen@gmail.com>
2017-11-16 11:47:07 -0500

    got a 4x speedup on gmsh parsing by switching to string_view where possible. there should be some headroom left for a faster string->double parser once gcc implements std::from_chars. Currently, the hand-rolled string_view->int parser is SUPER SUPER FRAGILE, and will not respond well to ill-formed input. Fortunately, Gmsh is pretty good, so user beware...

--- src/mesh/parse_gmsh.cpp
+++ src/mesh/parse_gmsh.cpp
@@ -5,12 +5,15 @@
 #include "utils/Range.hpp"
 #include <fstream>
 #include <string>
-#include <sstream>
+//#include <sstream>
+#include <string_view>
+//#include <charconv> //for std::from_chars, which is not yet in gcc as of v7.1
 
 YAFEL_NAMESPACE_OPEN
 
 static CellType gmsh_to_CellType(int);
 
+/*
 static void string_split(const std::string &s, char delim,
                          std::vector<std::string> &elems)
 {
@@ -20,7 +23,60 @@ static void string_split(const std::string &s, char delim,
         elems.push_back(word);
     }
 
-    return;
+}*/
+
+namespace {
+
+void string_split(std::string_view s, char delim,
+                  std::vector<std::string_view> &elems) {
+
+    auto pos = s.find_first_of(delim);
+    std::string::size_type start = 0;
+    while(pos != std::string::npos) {
+        if(pos != start) {
+            elems.push_back(s.substr(start, pos-start));
+        }
+        start = pos+1;
+
+        pos = s.find_first_of(delim, start);
+    }
+    if(start < s.length()) {
+        elems.push_back(s.substr(start, s.length()-start));
+    }
+
+}
+
+auto svtoi(std::string_view s) {
+    int val{0};
+    //This is the right way to do it in C++17, but gcc hasn't implemented it
+    //std::from_chars(s.data(), std::next(s.data(), s.size()), val);
+
+    //Super-fragile integer parsing, cannot handle badly-formatted input
+    //Evaluate using Horner's method for polynomial evaluation.
+    auto pos = s.data();
+    if(!pos) {
+        return val;
+    }
+    while(pos < s.end()) {
+
+        val = 10*val + (*pos - '0');
+        ++pos;
+    }
+    return val;
+}
+
+auto svtod(std::string_view s) {
+    double val{0};
+
+    //This is the right way to do it in C++17, but gcc hasn't implemented it
+    //std::from_chars(s.data(), std::next(s.data(), s.size()), val);
+
+    //Shitty fallback. Praying to the gods of small-string optimization here...
+    std::string S(s);
+    val = std::stod(S);
+
+    return val;
+}
 }
 
 
@@ -44,7 +100,7 @@ void Mesh::parse_gmsh(const std::string &fname)
 
     int offset{0};
     std::string line;
-    std::vector<std::string> words;
+    std::vector<std::string_view> words;
     while (!in.eof()) {
 
         std::getline(in, line);
@@ -84,26 +140,26 @@ void Mesh::parse_gmsh(const std::string &fname)
                 break;
 
             case PARSE_NODES: {
-                id = std::stoi(words[0]);
+                id = svtoi(words[0]);
                 coordinate<> node;
                 for (auto i : IRange(0,3)) {
-                    node(i) = std::stof(words[i + 1]);
+                    node(i) = svtod(words[i + 1]);
                 }
                 geometryNodes_[id - 1] = node;
                 break;
             }
             case PARSE_ELEMENTS: {
-                id = std::stoi(words[0]);
-                cellTypes_[id - 1] = gmsh_to_CellType(std::stoi(words[1]));
-                int ntags = std::stoi(words[2]);
+                id = svtoi(words[0]);
+                cellTypes_[id - 1] = gmsh_to_CellType(svtoi(words[1]));
+                int ntags = svtoi(words[2]);
                 int nodes_in_el = words.size() - ntags - 3;
                 std::vector<size_type> tag(ntags, 0);
                 for (int i = 3; i < 3 + ntags; ++i) {
-                    tag[i - 3] = std::stoi(words[i]);
+                    tag[i - 3] = svtoi(words[i]);
                 }
                 cellTags_[id - 1] = tag;
                 for (size_type i = 3 + ntags; i < static_cast<int>(words.size()); ++i) {
-                    cellNodes_.push_back(std::stoi(words[i]) - 1); //using 0-based node numbering from gmsh 1-based
+                    cellNodes_.push_back(svtoi(words[i]) - 1); //using 0-based node numbering from gmsh 1-based
                 }
                 cellOffsets_[id - 1] = offset;
                 offset += nodes_in_el;
