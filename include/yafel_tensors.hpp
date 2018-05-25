b00b9a874f5e8ea5ed69f6eb3ab2857f169b5f41
Tyler Olsen <tjolsen@mit.edu>
2018-05-25 00:49:25 -0400

    new file for convenience of using tensors in other projects

---
+++ include/yafel_tensors.hpp
@@ -0,0 +1,15 @@
+//
+// Created by tyler on 5/25/18.
+//
+
+#ifndef YAFEL_YAFEL_TENSORS_HPP
+#define YAFEL_YAFEL_TENSORS_HPP
+
+/**
+ * \file
+ * \brief Convenience header to only include the Tensor library.
+ * Useful in other projects when only using the Tensor library.
+ */
+#include "lin_alg/tensor/tensors.hpp"
+
+#endif //YAFEL_YAFEL_TENSORS_HPP
